47f36ab7713b711f4dd5038933be45de39cdeb3c
jrmncos <costillagermanalfredo@gmail.com>
2020-09-26 20:19:18 -0300

    first commit

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Sistema de seguimiento de Enfermdades Cronicas no transmisibles</Text>
       <StatusBar style="auto" />
     </View>
   );
