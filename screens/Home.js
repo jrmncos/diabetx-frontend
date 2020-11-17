6e21a58e7d8ac7a85b2d21d8c6d8c19308b02869
Santiago Galvan <santiagodgalvan@gmail.com>
2020-11-17 02:00:09 -0300

    seleccion de roles

--- screens/Home.js
+++ screens/Home.js
@@ -2,60 +2,110 @@ import React, { useState, useEffect } from 'react'
 import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 
-import { CheckBox, Divider } from 'react-native-elements';
-import doctor from 'assets/docto.png'
-import paciente from 'assets/abuelo.png'
+import { Divider } from 'react-native-elements';
 import {useAuth} from 'hooks/useAuth'
 import { useUser } from 'hooks/useUser';
 
+import paciente_f from 'imgUsuario/paciente_mujer.png'
+import paciente_m from 'imgUsuario/paciente_hombre.png'
+import profds_f from 'imgUsuario/pds_hombre.png'
+import profds_m from 'imgUsuario/pds_mujer.png'
+import corazon from 'recursos/corazon.png'
+
 export default function Home({navigation}){
-  const [opcionesRol, cambiarVistaOPCRol] = useState(true)
-  const [textoRol, setTextoRol] = useState("Profesional de la salud")
-  const [imagenRol, setImagenRol] = useState(doctor)
+  // const [opcionesRol, cambiarVistaOPCRol] = useState(true)
+  // const [textoRol, setTextoRol] = useState("Profesional de la salud")
+  // const [imagenRol, setImagenRol] = useState()
   const {logout, userToken} = useAuth()
   const {user} = useUser()
 
-  const [ isSelectingRole, setIsSelectingRole ] = useState()
-  /*
-  useEffect(()=>{
-    console.log("Effect del home")
-    console.log(user)
-  },[])
-  */
+  const [ isSelectingRole, setIsSelectingRole ] = useState(true)
+  const [ selectedRole, setSelectedRole ] = useState(null)
+  const [ iconSelectedRole, setIconSelectedRole ] = useState(corazon) 
+
+  const roles = [ "Paciente", "Profesional de Salud", "Promotor de Salud" ]
+
   const handleExit = () => {
     logout()
     navigation.navigate('Iniciar sesion')
   }
 
-  const cambiarTextos = () => {
-    if(opcionesRol){
-      setTextoRol("Paciente")
-      setImagenRol(paciente) 
+  function selectRole(rol){
+    console.log("Rol seleccionado: "+rol)
+    setIcon()
+    setSelectedRole(rol)
+    setIsSelectingRole(false)
+  }
+
+  function setIcon(){
+    console.log("genero: "+user.gender)
+    console.log("groups: "+user.groups)
+    console.log("selected rol"+selectedRole)
+    if(selectedRole == "Paciente"){
+      setIconSelectedRole((user.gender == "Femenino") ? paciente_f : paciente_m) 
+      // setIconSelectedRole((user.genero == "Femenino") ? 'imgUser/paciente_mujer.png' : 'imgUser/paciente_hombre.png') 
     }
-    else{
-      setTextoRol("Profesional de la salud")
-      setImagenRol(doctor)
+    else if(selectedRole == "Profesional de Salud"){
+      setIconSelectedRole((user.gender == "Femenino") ? profds_f : profds_m) 
+      //setIconSelectedRole((user.genero == "Femenino") ? 'imgUser/pds_mujer.png' : 'imgUser/pds_hombre.png')
     }
+    else if(selectedRole == "Promotor de Salud"){
+      setIconSelectedRole((user.gender == "Femenino") ? paciente_f : paciente_m) 
+    }
+    else
+      return setIconSelectedRole(corazon)
   }
 
   return(
     <View style={styles.container}>
-     
-      <View style={{flexDirection: 'row', alignSelf: 'center', width:"100%", backgroundColor: '#00a7ba'}}>
+      <TouchableOpacity 
+        style={{width:"100%", padding: "2%"}}       
+        >
+        <View style={styles.botonMenuHome}>
+          <Image
+            style={{ width: 50, height: 50, margin:"2%"}}
+            source={iconSelectedRole} 
+          />
+          <Text h2 style={styles.textoRol}>Tocame ( ͡° ͜ʖ ͡°)</Text> 
+        </View>
+      </TouchableOpacity>
+    {isSelectingRole && 
+    <>
+    <Text h2 style={styles.textoBienvenida}>Bienvenido!</Text>
+      {user &&<Text h2 style={styles.textoNombreUsuario}>{user.first_name+" "+user.last_name}</Text>}
+      {/* {user && <Text h2 style={styles.textoNombreUsuario}>{user.dni}</Text>}
+      {userToken && <Text h2 style={styles.textoNombreUsuario}>{userToken}</Text>} */}
+    <Text h2 style={styles.textoBienvenida}>Seleccione un rol para continuar</Text>
+    {user.groups.map(rol => 
+      {return (<TouchableOpacity 
+        key={rol}
+        style={{width:"100%", padding: "2%"}}       
+        onPress={() => selectRole(rol)}>
+        <View style={styles.botonMenuHome}>
+          <Image
+            style={{ width: 50, height: 50, margin:"2%"}}
+            // source={require('../assets/locationGeo.png')} 
+          />
+          <Text h2 style={styles.textoRol}>{rol}</Text> 
+        </View>
+      </TouchableOpacity>)})}
+      
+    </>} 
+    {!isSelectingRole && 
+    <>
+    <View style={{flexDirection: 'row', alignSelf: 'center', width:"100%", backgroundColor: '#00a7ba'}}>
           <Image
             style={{ width: 70, height: 70, backgroundColor:"#00a7ba"}}
-            source={imagenRol} 
+            source={iconSelectedRole} 
           />
-          <Text h2 style={styles.textoRol}>{textoRol}</Text> 
+          <Text h2 style={styles.textoRol}>{selectedRole}</Text> 
       </View>
-
-      <Text h2 style={styles.textoBienvenida}>Bienvenido!</Text>
-      {user &&<Text h2 style={styles.textoNombreUsuario}>{user.first_name}</Text>}
-      {user && <Text h2 style={styles.textoNombreUsuario}>{user.dni}</Text>}
-      {userToken && <Text h2 style={styles.textoNombreUsuario}>{userToken}</Text>}
-      <Text h2 style={styles.textoBienvenida}>Selecciona una acción para continuar.</Text> 
-
-      <View>
+    <Text h2 style={styles.textoBienvenida}>Bienvenido!</Text>
+      {user &&<Text h2 style={styles.textoNombreUsuario}>{user.first_name+" "+user.last_name}</Text>}
+      {/* {user && <Text h2 style={styles.textoNombreUsuario}>{user.dni}</Text>}
+      {userToken && <Text h2 style={styles.textoNombreUsuario}>{userToken}</Text>} */}
+    <Text h2 style={styles.textoBienvenida}>Seleccione un rol para continuar</Text>
+      {/* <View>
         <CheckBox
           title={<Text>Cambiar vista: Paciente/Profesional</Text>}
           checked={opcionesRol}
@@ -63,12 +113,10 @@ export default function Home({navigation}){
             cambiarTextos()
             cambiarVistaOPCRol(!opcionesRol)}}
         />
-      </View>
-
+      </View> */}
       <Divider style={styles.divisorInferior} />
-      
       {
-        opcionesRol && 
+        selectedRole == "Profesional de Salud" && 
         <>
         <TouchableOpacity 
           style={{width:"100%", padding: "2%"}}       
@@ -109,7 +157,7 @@ export default function Home({navigation}){
         
       }
       {
-        !opcionesRol &&
+        selectedRole == "Paciente" &&
         <>
       <TouchableOpacity 
         style={{width:"100%", padding: "2%"}}       
@@ -117,7 +165,7 @@ export default function Home({navigation}){
         <View style={styles.botonMenuHome}>
           <Image
             style={{ width: 60, height: 60, margin:"1%"}}
-            source={require('../assets/abuelos.png')} 
+            source={iconSelectedRole} 
           />
           <Text h2 style={styles.textoRol}>Perfil de usuario</Text> 
         </View>
@@ -143,13 +191,14 @@ export default function Home({navigation}){
         <View style={styles.botonSalir}>
           <Image
             style={{ width: 50, height: 50, margin:"2%"}}
-            source={require('../assets/archivo-medico.png')} 
+            source={require('../assets/cerrar-sesion.png')} 
           />
           <Text h2 style={styles.textoRol}>Salir</Text> 
         </View>
       </TouchableOpacity>
       
       <StatusBar style="auto" />
+    </>}
     </View>
     )
   
@@ -182,9 +231,9 @@ const styles = StyleSheet.create({
       flexDirection: 'row', 
       alignSelf: 'center', 
       width:"100%", 
-      backgroundColor: '#fcad03',
+      backgroundColor: '#00a7ba',
       borderWidth: 1,
-      borderColor: "#d99400",
+      borderColor: "#00707d",
       shadowColor: 'rgba(0, 0, 0, 1)',
       shadowOpacity: 1,
       elevation: 5,
